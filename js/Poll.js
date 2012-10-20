37a2e9b446874113cec683d9528899525a87c70d
Public Keating <publickeating@me.com>
2012-10-20 17:18:41 -0600

    Adds code to get lookup table, find the poll data for the current position and get the poll data.

--- js/Poll.js
+++ js/Poll.js
@@ -12,25 +12,37 @@
             $poll_map_container: null
         },
 
-        init: function(opts){
-
+        init: function(opts) {
             var self = this;
 
             self.opts = $.extend(true, {}, self.defaults, opts);
 
             if (navigator && navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position){
-                    self.getClosestPoll(position.coords.latitude, position.coords.longitude);
-                }, self.geoError);
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    // Wrap our call to fix context of `this`.
+                    self.didGetCurrentPosition(position);
+                },
+                self.geoError);
             } else {
                 self.geocodeNotSupported(function(position){
                     self.getClosestPoll(position.latitude, position.longitude);
                 });
                 //alert('Your browser does not support geolocation');
             }
+        },
 
             self.assignEvents();
 
+        /**
+            Callback for navigator.geolocation success.
+        */
+        didGetCurrentPosition: function (position) {
+            var self = this;
+
+            this.getPollLookupTable(function(lookupTable) {
+                // Wrap our call to fix context of `this`.
+                self.didGetPollLookupTable(position, lookupTable);
+            });
         },
 
         assignEvents: function(){
@@ -89,6 +101,15 @@
 */
 
         geoError: function(err){
+        /**
+            Gets the poll data lookup table.   This table is used to find the
+            name of the poll data for our current location.
+        */
+        getPollLookupTable: function (cb) {
+            $.getJSON('data/poll_table.json', function (data) {
+                cb(data);
+            });
+        },
 
             if(err.code == 1) {
                 // alert('The user denied the request for location information.')
@@ -102,14 +123,43 @@
             } else {
                 alert('An unknown error occurred while requesting your location.')
             }
-
-        },
-
-        getClosestPoll: function(lat, lng){
-
+        /**
+            Callback for poll lookup success.
+        */
+        didGetPollLookupTable: function (position, lookupTable) {
             var self = this;
 
+            this.getPollData(position, lookupTable, function(pollData) {
+                // Wrap our call to fix context of `this`.
+                self.didGetPollData(position, pollData);
+            });
+        },
+
+        /**
+            Uses the poll data lookup table to find the poll data for our
+            position.
+        */
+        getPollData: function (position, lookupTable, cb) {
+            var fileName;
+
+            fileName = this.filenameForPosition(position, lookupTable);
+            if (fileName) {
+                debugger;
+                $.getJSON('data/' + fileName, function(data) {
+                    cb(data);
+                });
+            } else {
+                console.error('no matching poll data file found');
+            }
+        },
+
+        /**
+            Callback for poll data success.
+        */
+        didGetPollData: function (position, pollData) {
             var directionsDisplay = new google.maps.DirectionsRenderer();
+            var lat = position.coords.latitude,
+                lng = position.coords.longitude;
             var start = new google.maps.LatLng(lat, lng);
             var mapOptions = {
                 zoom: 7,
@@ -117,8 +167,8 @@
                 center: start
             };
 
-            self.gmap = new google.maps.Map(self.opts.$map[0], mapOptions);
-            directionsDisplay.setMap(self.gmap);
+            this.gmap = new google.maps.Map(this.opts.$map[0], mapOptions);
+            directionsDisplay.setMap(this.gmap);
 
             var directionsService = new google.maps.DirectionsService();
             var end = 'Regina, SK';
@@ -129,14 +179,98 @@
             };
 
             directionsService.route(request, function(response, status) {
-                if (status == google.maps.DirectionsStatus.OK) {
+                if (status === google.maps.DirectionsStatus.OK) {
                     directionsDisplay.setDirections(response);
                     self.postMapRender();
                 }
             });
+        },
+
+        /**
+            Parses the lookup table to find a filename of poll data for our
+            current position.
+        */
+        filenameForPosition: function (position, lookupTable) {
+            var dist, i,
+                ret = null,
+                // sourceLat = position.lat,
+                // sourceLong = position.coordinates.lng,
+                point = { x: position.coords.longitude, y: position.coords.latitude };
+
+            for (i = lookupTable.length - 1; i >= 0; i--) {
+                var pollDataFileInfo = lookupTable[i],
+                    rect = {};
+
+                rect.x = pollDataFileInfo.topLeft.lng;
+                rect.y = pollDataFileInfo.botRight.lat;
+                rect.width = pollDataFileInfo.botRight.lng - rect.x;
+                rect.height = pollDataFileInfo.topLeft.lat - rect.y;
+
+                if (this.pointInRect(point, rect)) {
+                    ret = pollDataFileInfo.filename;
+                }
+            }
+
+            return ret;
+        },
+
+        /**
+            Returns if a point is in a rect.
+        */
+        pointInRect: function(point, rect) {
+            return (point.x >= this.minX(rect)) &&
+                (point.y >= this.minY(rect)) &&
+                (point.x <= this.maxX(rect)) &&
+                (point.y <= this.maxY(rect));
+        },
+
+        /** Return the left edge of the rect */
+        minX: function(rect) {
+            return rect.x || 0;
+        },
+
+        /** Return the right edge of the rect. */
+        maxX: function(rect) {
+            return (rect.x || 0) + (rect.width || 0);
+        },
+
+        /** Return the top edge of the rect */
+        minY: function(rect) {
+            return rect.y || 0;
+        },
+
+        /** Return the bottom edge of the rect */
+        maxY: function(rect) {
+            return (rect.y || 0) + (rect.height || 0);
+        },
+
+        /**
+          Calculate the approximate distance between two geographic locations
+          using the dist cosine law.
+
+          @returns {Number}
+        */
+        distance: function(sourceLat, sourceLong, destLat, destLong) {
+          var radius = 6378137; // earth's mean radius in m
+          var result = Math.acos(Math.sin(sourceLat.toRad()) * Math.sin(destLat.toRad()) + Math.cos(sourceLat.toRad()) * Math.cos(destLat.toRad()) * Math.cos((destLong - sourceLong).toRad())) * radius;
+
+          return result;
+        },
+
+        geoError: function(err){
+
+            if(err.code === 1) {
+                alert('The user denied the request for location information.');
+            } else if(err.code === 2) {
+                alert('Your location information is unavailable.');
+            } else if(err.code === 3) {
+                alert('The request to get your location timed out.');
+            } else {
+                alert('An unknown error occurred while requesting your location.');
+            }
 
         }
 
-    }
+    };
 
 })(window, undefined);
